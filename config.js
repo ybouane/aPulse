879204788c734ad25c02b08dc5a418194ac5d6ce
ybouane <yassine@bouanane.com>
2024-10-09 10:37:51 -0400

    Implemented config.js

--- config.js
+++ config.js
@@ -1,8 +1,24 @@
 export default {
-	"port"	: 2600,
-	"sites"	: [
+	verbose				: true,
+	interval			: 15, // In minutes
+	responseTimeGood	: 300, // In milliseconds, this and below will be green
+	responseTimeWarning	: 600, // In milliseconds, above this will be red
+	timeout				: 5000, // In milliseconds, requests will be aborted above this. (Do not )
+	sites	: [
 		{
-			
+			id			: 'google',
+			name		: 'Google',
+			endpoints	: [
+				{
+					name	: 'Homepage',
+					url	: 'https://www.google.com',
+					request	: {
+						method: 'GET',
+					},
+					mustFind	: 'Feeling Lucky',
+					mustNotFind	: /not found/i,
+				}
+			]
 		}
 	],
 };
