c0db0a4e27c7a0ba04b12a14d8a8916621575122
ybouane <yassine@bouanane.com>
2024-10-09 14:26:17 -0400

    Better config file.

--- config.js
+++ config.js
@@ -4,19 +4,24 @@ export default {
 	responseTimeGood	: 300, // In milliseconds, this and below will be green
 	responseTimeWarning	: 600, // In milliseconds, above this will be red
 	timeout				: 5000, // In milliseconds, requests will be aborted above this. (Do not )
-	sites	: [
+	readableStatusJson	: true, // Format status.json to be human readable
+	logsMaxDatapoints	: 200, // Maximum datapoints history to keep (per endpoint)
+	sites				: [
 		{
-			id			: 'google',
-			name		: 'Google',
-			endpoints	: [
+			id				: 'google', // optional
+			name			: 'Google',
+			endpoints		: [
 				{
-					name	: 'Homepage',
-					url	: 'https://www.google.com',
-					request	: {
+					id				: 'homepage', // optional
+					name			: 'Homepage',
+					url				: 'https://www.google.com',
+					request			: {
 						method: 'GET',
 					},
-					mustFind	: 'Feeling Lucky',
-					mustNotFind	: /not found/i,
+					mustFind		: 'Feeling Lucky', // String | Regex | Function | AsyncFunction
+					mustNotFind		: /not found/i,
+					followRedirects	: true,
+					validStatus		: [200],
 				}
 			]
 		}
